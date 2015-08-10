3a5c3b1ef89f3dd4d73a9c1f9238bbab7e840355
xjpvictor Huang <ke@xjpvictor.info>
2015-08-10 21:55:45 +0800

    Add thumbnail to images in attachment list

--- functions.php
+++ functions.php
@@ -478,9 +478,9 @@ function displayattachment($id, $attachment, $tmp = 0, $post = 0) {
   if (!isset($id) || !isset($attachment) || !is_array($attachment))
     return false;
 
-  return '<div id="attachment-'.$id.'-'.$attachment['t'].'">
+  return '<div id="attachment-'.$id.'-'.$attachment['t'].'"'.($attachment['type'] == '1' ? ' class="image" style="background-image:url(\'attachment.php?id='.$id.'&name='.$attachment['url_name'].'&action=get'.($tmp ? '&tmp=1' : '').'\');"' : '').'>
     <span class="attachment">
-      <a href="attachment.php?id='.$id.'&name='.$attachment['url_name'].'&action=get'.($tmp ? '&tmp=1' : '').'" target="_blank">'.$attachment['display_name'].'</a>'.($attachment['type'] == '1' && $post ? '&nbsp;&nbsp;
+      <a href="attachment.php?id='.$id.'&name='.$attachment['url_name'].'&action=get'.($tmp ? '&tmp=1' : '').'" target="_blank">'.$attachment['display_name'].'</a>'.($id && $attachment['type'] == '1' && $post ? '&nbsp;&nbsp;
       <span class="insert" onclick="mdAddHR(\'!['.$attachment['display_name'].'](attachment.php?id='.$id.'&name='.$attachment['url_name'].'&action=get'.($tmp ? '&tmp=1' : '').' &quot;'.$attachment['display_name'].'&quot;)\')">Insert</span>' : '').'
     </span>'.($post ? '<span class="delete" onclick="deleteAttachment(\''.$id.'\', \''.$attachment['url_name'].'\', \'attachment-'.$id.'-'.$attachment['t'].'\')">&#10007;</span>' : '').'</div>';
 }
