e93af7a121c87c5f17d3e3f5abbd383585cc59c3
xjpvictor Huang <ke@xjpvictor.info>
2016-08-30 19:30:31 +0800

    Ignore img with empty src

--- functions.php
+++ functions.php
@@ -79,7 +79,7 @@ function escpost($str, $id, $source, $edit = 0) {
     //"  \n",
   );
   $str = preg_replace($search, $replace, $str);
-  $str = preg_replace_callback('/<img ((?:[^>]*\s)*)src\s*=\s*("|\')(?!'.$url.'|attachment\.php)([^"\']*)("|\')(\s+[^>]*)?(\/\s*)?>/i', function ($match) use ($id) {
+  $str = preg_replace_callback('/<img ((?:[^>]*\s)*)src\s*=\s*("|\')(?!'.$url.'|attachment\.php)([^"\']+)("|\')(\s+[^>]*)?(\/\s*)?>/i', function ($match) use ($id) {
     return '<img '.$match[1].'src='.$match[2].'attachment.php?id='.$id.'&cache='.rawurlencode($match[3]).$match[4].(isset($match[5]) ? $match[5] : '').(isset($match[6]) ? $match[6] : '/').'>';
   }, $str);
 
